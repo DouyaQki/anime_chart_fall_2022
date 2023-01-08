b60ba42ef568fdf36bdeec74b1af4fbebc9f7196
DouyaQki <douyaqki@gmail.com>
2023-01-08 19:24:26 -0300

    search input added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './App.css'
+import randomImg from './assets/img/arknights_reimei_zensou.jpg'
 
 const App = () => {
+
+  const getData = (url) => {
+
+  }
   return (
-    <div>App</div>
+    <div>
+      <input type='search' className='search' placeholder='Filter anime' />
+
+      <h2>TV</h2>
+      <main>
+        <img src={randomImg} alt="arknights" />
+      </main>
+    </div>
   )
 }
 
