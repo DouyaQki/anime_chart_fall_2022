b8c486c1a5c1567827b8263453cfcb4a1fad2520
DouyaQki <douyaqki@gmail.com>
2023-01-09 15:17:31 -0300

    chart json changed to public fodlfolder.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,34 @@ import randomImg from './assets/img/arknights_reimei_zensou.jpg'
 
 const App = () => {
 
-  const getData = (url) => {
+  const URL = "https://douyaqki.github.io/anime_chart_fall_2022_js/chart.json"
+  
+  const getData = async (url) => {
+    try {
+      const response = await fetch(url)
 
+      if(response.ok) {
+        const data = await response.json()
+        return data
+      }
+
+      throw Error('something happened')
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  getData(URL)
+    .then(res => console.log(res))
+
+
   return (
     <div>
       <input type='search' className='search' placeholder='Filter anime' />
 
       <h2>TV</h2>
       <main>
-        <img src={randomImg} alt="arknights" />
+        <img src={randomImg} alt='arknights' height="300px" />
       </main>
     </div>
   )
