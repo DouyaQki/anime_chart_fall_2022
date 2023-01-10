1a8e2c68034b1d014dbeaa099275c703a1d8fe22
DouyaQki <douyaqki@gmail.com>
2023-01-09 22:14:10 -0300

    modeling boxes.

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import arkImg from '../../public/img/arknights_reimei_zensou.jpg'
+import arkImg from './arknights_reimei_zensou.jpg'
 import '../CardImage.css'
 
 const CardImage = ({img, title, studio}) => {
   return (
     <div className='image-container'>
-      <img className='card-img' src={arkImg} alt="" />
+      <img className='card-img' src="" alt="" />
 
       <div className='card-img-details-container'>
         <p className='card-title'>title</p>
