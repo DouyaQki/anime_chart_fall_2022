08951cd2f3a248af021e82e0c38d35954b8807bc
DouyaQki <douyaqki@gmail.com>
2023-01-11 12:42:28 -0300

    testing the onload event.

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../CardImage.css'
 
-const CardImage = ({img, title, studio}) => {
+const CardImage = ({ img, title, studio }) => {
+  const imgLoading =
+    'https://douyaqki.github.io/anime_chart_fall_2022/img/img_loading.png'
+
+  const handleImageLoad = (e) => {
+    e.target.src = img
+  }
+
   return (
     <div className='image-container'>
-      <img className='card-img' src={img} alt={img} loading='lazy' />
+      <img
+        className='card-img'
+        src={imgLoading}
+        alt=''
+        loading='lazy'
+        onLoad={handleImageLoad}
+        height='240'
+        width='160'
+      />
 
       <div className='card-img-details-container'>
         <span className='card-title'>{title}</span>
